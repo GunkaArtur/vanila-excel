f9281c9f85c6ddeb985496545f164ec367c69bc5
Artur Gunka <arturel101010@mail.ru>
2020-08-28 17:11:17 +0300

    Added html and dom library

--- src/index.js
+++ src/index.js
@@ -10,4 +10,4 @@ const excel = new Excel("#app", {
 });
 
 excel.render();
-//TODO: 3.03
+//TODO: 3.04
--- webpack.config.js
+++ webpack.config.js
@@ -4,44 +4,46 @@ const HTMLWebpackPlugin = require("html-webpack-plugin");
 const CopyPlugin = require("copy-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
-const isProd = process.env.NODE_ENV === "production"
-const isDev = !isProd
+const isProd = process.env.NODE_ENV === "production";
+const isDev = !isProd;
 
-const filename = ext => isDev ? `bundle.${ext}` : `bundle.[hash].${ext}`
+const filename = ext => (isDev ? `bundle.${ext}` : `bundle.[hash].${ext}`);
 
-console.log("is prod",isProd)
-console.log("is dev",isDev)
+console.log("is prod", isProd);
+console.log("is dev", isDev);
 
-const jsLoaders = ()=> {
+const jsLoaders = () => {
   const loaders = [
     {
       loader: "babel-loader",
       options: {
         presets: ["@babel/preset-env"],
-      },
+        plugins: ["@babel/plugin-proposal-class-properties"]
+      }
     }
- ]
+  ];
 
-  if(isDev){
-    loaders.push("eslint-loader")
-
+  if (isDev) {
+    loaders.push("eslint-loader");
   }
-}
+
+  return loaders;
+};
 
 module.exports = {
   context: path.resolve(__dirname, "src"),
   mode: "development",
-  entry: ["@babel/polyfill","./index.js"],
+  entry: ["@babel/polyfill", "./index.js"],
   output: {
     filename: filename("js"),
-    path: path.resolve(__dirname, "dist"),
+    path: path.resolve(__dirname, "dist")
   },
   resolve: {
     extensions: [".js"],
     alias: {
       "@": path.resolve(__dirname, "src"),
-      "@core": path.resolve(__dirname, "src/core"),
-    },
+      "@core": path.resolve(__dirname, "src/core")
+    }
   },
   devtool: isDev ? "source-map" : false,
   devServer: {
@@ -61,31 +63,35 @@ module.exports = {
       patterns: [
         {
           from: path.resolve(__dirname, "src/favicon.ico"),
-          to: path.resolve(__dirname, "dist"),
-        },
-      ],
+          to: path.resolve(__dirname, "dist")
+        }
+      ]
     }),
     new MiniCssExtractPlugin({
-      filename: filename("css"),
-    }),
+      filename: filename("css")
+    })
   ],
   module: {
     rules: [
       {
         test: /\.s[ac]ss$/i,
-        use: [{
-          loader: MiniCssExtractPlugin.loader,
-          options: {
-            hmr: isDev,
-            reloadAll: true
-          }
-        }, "css-loader", "sass-loader"],
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              hmr: isDev,
+              reloadAll: true
+            }
+          },
+          "css-loader",
+          "sass-loader"
+        ]
       },
       {
         test: /\.js$/,
         exclude: /node_modules/,
-        use: jsLoaders(),
-      },
-    ],
-  },
+        use: jsLoaders()
+      }
+    ]
+  }
 };
