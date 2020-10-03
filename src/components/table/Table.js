0ceb1a9e01fee7b4471b4ec59d355f0e5e484bd8
Artur Gunka <arturel101010@mail.ru>
2020-10-03 16:23:02 +0300

    col resize

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -1,12 +1,13 @@
 import { ExcelComponent } from "../../core/ExcelComponent";
 import { createTable } from "./table.template";
+import { $ } from "../../core/dom";
 
 export class Table extends ExcelComponent {
   static className = "excel__table";
 
   constructor($root) {
     super($root, {
-      listeners: ["mousedown"],
+      listeners: ["mousedown"]
     });
   }
 
@@ -16,7 +17,22 @@ export class Table extends ExcelComponent {
 
   onMousedown(event) {
     if (event.target.dataset.resize) {
-      console.log(event.target.dataset.resize);
+      console.log(event.target);
+      const $resizer = $(event.target);
+      // const $parent = $resizer.$el.parentNode; //bad!
+      // const $parent = $resizer.$el.closest(`.column`); // better but still bad
+      const $parent = $resizer.closest(`[data-type="resizable"]`);
+      const coords = $parent.getCoords();
+
+      document.onmousemove = e => {
+        const delta = e.pageX - coords.right;
+        const value = coords.width + delta;
+        $parent.$el.style.width = value + "px";
+      };
+
+      document.onmouseup = () => {
+        document.onmousemove = null;
+      };
     }
   }
 }
--- src/components/table/table.template.js
+++ src/components/table/table.template.js
@@ -1,6 +1,6 @@
 const CODES = {
   A: 65,
-  Z: 90,
+  Z: 90
 };
 
 function toCell() {
@@ -8,7 +8,7 @@ function toCell() {
 }
 
 function toColumn(col) {
-  return `<div class="column">
+  return `<div class="column" data-type="resizable">
       ${col}
       <div class="col-resize" data-resize="col"></div>
       </div>`;
@@ -18,7 +18,7 @@ function createRow(index, content) {
   const resizer = index
     ? `<div class="row-resize" data-resize="row"></div>`
     : "";
-  return ` <div class="row">
+  return ` <div class="row" data-type="resizable">
         <div class="row-info">
             ${index ? index : ""}
             ${resizer}
@@ -39,12 +39,19 @@ export function createTable(rowsCount = 15) {
 
   const rows = [];
 
-  const cols = new Array(colsCount).fill("").map(toChar).map(toColumn).join("");
+  const cols = new Array(colsCount)
+    .fill("")
+    .map(toChar)
+    .map(toColumn)
+    .join("");
 
   rows.push(createRow(null, cols));
 
   for (let i = 0; i < rowsCount; i++) {
-    const cells = new Array(colsCount).fill("").map(toCell).join("");
+    const cells = new Array(colsCount)
+      .fill("")
+      .map(toCell)
+      .join("");
     rows.push(createRow(i + 1, cells));
   }
 
