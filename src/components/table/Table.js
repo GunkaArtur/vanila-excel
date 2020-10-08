bfe6f23af3c2c083d0f2721c8e1a5e8e62a6eb40
Artur Gunka <arturel101010@mail.ru>
2020-10-08 13:39:31 +0300

    Select realized

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -1,8 +1,9 @@
 import { ExcelComponent } from "../../core/ExcelComponent";
 import { createTable } from "./table.template";
 import { resizeHandler } from "./table.resize";
-import { shouldResize } from "./tableFunctions";
+import { isCell, shouldResize } from "./tableFunctions";
 import { TableSelection } from "./TableSelection";
+import { $ } from "../../core/dom";
 
 export class Table extends ExcelComponent {
   static className = "excel__table";
@@ -31,6 +32,9 @@ export class Table extends ExcelComponent {
   onMousedown(event) {
     if (shouldResize(event)) {
       resizeHandler(this.$root, event);
+    } else if (isCell(event)) {
+      const target = $(event.target);
+      this.selection.select(target);
     }
   }
 }
--- src/components/table/TableSelection.js
+++ src/components/table/TableSelection.js
@@ -1,12 +1,20 @@
 export class TableSelection {
+  static classname = "selected";
+
   constructor() {
     this.group = [];
   }
 
   // $el instanceof DOM === true
   select($el) {
+    this.clear();
     this.group.push($el);
-    $el.addClass("selected");
+    $el.addClass(TableSelection.classname);
+  }
+
+  clear() {
+    this.group.forEach($el => $el.removeClass(TableSelection.classname));
+    this.group = [];
   }
 
   selectGroup() {}
